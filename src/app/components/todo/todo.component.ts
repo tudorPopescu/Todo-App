0d5f6302690e241266854fd95f8c37d9d7550d0f
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-05-08 13:14:01 +0300

    Created addTodo method

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -8,9 +8,39 @@ import { List } from '../../interfaces/list';
 })
 export class TodoComponent implements OnInit {
   todos: List[];
+  todoId: number;
+  todoTitle: string;
 
   constructor() { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.todoId = 3;
+    this.todoTitle = '';
+
+    this.todos = [
+      {
+        id: 1,
+        title: 'First todo'
+      },
+      {
+        id: 2,
+        title: 'Second todo'
+      }
+    ];
+  }
+
+  addTodo() {
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+
+    this.todos.push({
+      id: this.todoId,
+      title: this.todoTitle
+    });
+
+    this.todoId++;
+    this.todoTitle = '';
+  }
 
 }
