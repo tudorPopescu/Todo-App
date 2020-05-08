011c3c6eaa0d0b2be724cf2cf1803389bf3fd6b7
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-05-08 13:03:49 +0300

    Created list interface

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from '../../interfaces/list';
 
 @Component({
   selector: 'app-todo',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo.component.scss']
 })
 export class TodoComponent implements OnInit {
+  todos: List[];
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void { }
 
 }
