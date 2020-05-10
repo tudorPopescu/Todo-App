7be91a65517893eadeb06b6be34ac3607b6effd8
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-05-10 12:03:48 +0300

    Moved all existing code into a service

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { List } from '../../interfaces/list';
 import { TodoListService } from './../../services/todo-list.service';
 
 @Component({
@@ -9,22 +8,14 @@ import { TodoListService } from './../../services/todo-list.service';
   providers: [TodoListService]
 })
 export class TodoComponent implements OnInit {
-  todos: List[];
-  todoId: number;
   todoTitle: string;
   todoCompleted: boolean;
-  editable: boolean;
-  filter: string;
 
-  constructor(private todoListService: TodoListService) { }
+  constructor(public todoList: TodoListService) { }
 
   ngOnInit(): void {
-    this.todos = this.todoListService.todosList;
-    this.todoId = 3;
     this.todoTitle = '';
     this.todoCompleted = false;
-    this.editable = false;
-    this.filter = 'all';
   }
 
   addTodo(): void {
@@ -32,55 +23,16 @@ export class TodoComponent implements OnInit {
       return;
     }
 
-    this.todos.push({
-      id: this.todoId,
-      title: this.todoTitle,
-      completed: this.todoCompleted,
-      edit: this.editable
-    });
+    this.todoList.addTodo(this.todoTitle, this.todoCompleted);
 
-    this.todoId++;
     this.todoTitle = '';
   }
 
-  removeTodo(todoId: number) {
-    this.todos = this.todos.filter(todo => todoId !== todo.id);
-  }
-
-  getItems(): number {
-    return this.todos.filter(todo => !todo.completed).length;
-  }
-
   checkAll(): void {
-    this.todos.forEach(todo => (todo.completed = this.todoCompleted));
-  }
-
-  editTodoTitle(todo): boolean {
-    return todo.edit = !this.editable;
-  }
-
-  cancelEditTitle(todo): boolean {
-    return todo.edit = this.editable;
-  }
-
-  updateTitle(todo: List, data: Event): boolean {
-    todo.title = (data.target as HTMLInputElement).value;
-    return todo.edit = this.editable;
+    this.todoList.checkAll(!this.todoCompleted);
   }
 
   clearCompleted(): void {
-    this.todos = this.todos.filter(todo => todo.completed === this.todoCompleted);
-  }
-
-  filterTodo(): List[] {
-    if (this.filter === 'all') {
-      return this.todos;
-    } else if (this.filter === 'active') {
-      return this.todos.filter(todo => !todo.completed);
-    } else if (this.filter === 'completed') {
-      return this.todos.filter(todo => todo.completed);
-    }
-
-    return this.todos;
+    this.todoList.clearCompleted(this.todoCompleted);
   }
 }
--- src/app/services/todo-list.service.ts
+++ src/app/services/todo-list.service.ts
@@ -5,10 +5,17 @@ import { List } from '../interfaces/list';
   providedIn: 'root'
 })
 export class TodoListService {
-todosList: List[];
+  todos: List[];
+  todoId: number;
+  editable: boolean;
+  filter: string;
 
   constructor() {
-    this.todosList = [
+    this.todoId = 3;
+    this.editable = false;
+    this.filter = 'all';
+
+    this.todos = [
       {
         id: 1,
         title: 'Learn Angular',
@@ -34,6 +41,57 @@ todosList: List[];
         edit: false
       },
     ];
-    console.log(this.todosList);
+  }
+
+  addTodo(todoTitle: string, todoCompleted: boolean): void {
+    this.todos.push({
+      id: this.todoId,
+      title: todoTitle,
+      completed: todoCompleted,
+      edit: this.editable
+    });
+
+    this.todoId++;
+  }
+
+  removeTodo(todoId: number) {
+    this.todos = this.todos.filter(todo => todoId !== todo.id);
+  }
+
+  getItems(): number {
+    return this.todos.filter(todo => !todo.completed).length;
+  }
+
+  checkAll(todoCompleted: boolean): void {
+    this.todos.forEach(todo => (todo.completed = !todoCompleted));
+  }
+
+  editTodoTitle(todo): boolean {
+    return todo.edit = !this.editable;
+  }
+
+  cancelEditTitle(todo): boolean {
+    return todo.edit = this.editable;
+  }
+
+  updateTitle(todo: List, data: Event): boolean {
+    todo.title = (data.target as HTMLInputElement).value;
+    return todo.edit = this.editable;
+  }
+
+  clearCompleted(todoCompleted: boolean): void {
+    this.todos = this.todos.filter(todo => todo.completed === todoCompleted);
+  }
+
+  filterTodo(): List[] {
+    if (this.filter === 'all') {
+      return this.todos;
+    } else if (this.filter === 'active') {
+      return this.todos.filter(todo => !todo.completed);
+    } else if (this.filter === 'completed') {
+      return this.todos.filter(todo => todo.completed);
+    }
+
+    return this.todos;
   }
 }
